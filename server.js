c7b76026b863d1864ae5733835dd71e62c9fc668
fatima <fatimafouad9999@gmail.com>
2024-03-22 20:33:57 +0300

    connection to the db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-app.use(cors({ origin: "http://localhost:8100" }));
+app.use(cors({ origin: "http://localhost:3306" }));
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 app.use(bodyParser.json());
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 var dbConn = mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "root",
+      password: "password",
       database: "talent"
 });
 app.use(function (req, res, next) {
@@ -27,7 +27,12 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+app.get('/users', function (req, res) {
+      dbConn.query('SELECT * FROM users', function (error, results, fields) {
+            if (error) throw error;
+            return res.send({ error: false, data: results, message: 'users list.' });
+      });
+});
 
 app.listen(3000, function () {
     console.log('Node app is running on port 3000');
