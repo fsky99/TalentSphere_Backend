a17b61ed70768fe6f9ac59aca046c063cfdd9237
shaikha <jshaikha0@gmail.com>
2024-03-22 20:55:01 +0300

    localhost

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-app.use(cors({ origin: "http://localhost:8100" }));
-var bodyParser = require('body-parser');
 var mysql = require('mysql');
+app.use(cors({ origin: "http://localhost:3306" }));
+var bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
       extended: true
@@ -19,17 +20,34 @@ app.get('/', function (req, res) {
 var dbConn = mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "root",
+      password: "password",
       database: "talent"
 });
+
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+app.get('/users', function (req, res) {
+      dbConn.query('SELECT * FROM users', function (error, results, fields) {
+            if (error) throw error;
+            return res.send({ error: false, data: results, message: 'users list.' });
+      });
+});
 
 app.listen(3000, function () {
     console.log('Node app is running on port 3000');
 });
+
 module.exports = app;
+
+
+
+
+
+
+
+
+
