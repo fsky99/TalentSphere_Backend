363ec6288e64fbd0e3dfecdb9d70f408d46b7ce0
fatima <fatimafouad9999@gmail.com>
2024-03-23 03:16:20 +0300

    rest of insert

--- server.js
+++ server.js
@@ -179,16 +179,12 @@ app.post("/addUser", function (req, res) {
     !address ||
     !gender
   ) {
-    return res
-      .status(400)
-      .send({
-        error: true,
-        message: "Please provide all required user details",
-      })
+    return res.status(400).send({
+      error: true,
+      message: "Please provide all required user details",
+    })
   }
 
-  // Hash the password before inserting it into the database
-  // For example, using bcrypt
   bcrypt.hash(password, 10, function (err, hashedPassword) {
     if (err) {
       return res
@@ -215,12 +211,10 @@ app.post("/addUser", function (req, res) {
       },
       function (error, results, fields) {
         if (error) {
-          return res
-            .status(500)
-            .send({
-              error: true,
-              message: "Error inserting user into database",
-            })
+          return res.status(500).send({
+            error: true,
+            message: "Error inserting user into database",
+          })
         }
         return res.send({
           error: false,
@@ -236,12 +230,10 @@ app.post("/addEvent", function (req, res) {
   const { userID, eventName, eventDate, eventTime, picture } = req.body
 
   if (!userID || !eventName || !eventDate || !eventTime) {
-    return res
-      .status(400)
-      .send({
-        error: true,
-        message: "Please provide all required event details",
-      })
+    return res.status(400).send({
+      error: true,
+      message: "Please provide all required event details",
+    })
   }
 
   dbConn.query(
@@ -266,12 +258,10 @@ app.post("/addSalary", function (req, res) {
   const { userID, salary } = req.body
 
   if (!userID || !salary) {
-    return res
-      .status(400)
-      .send({
-        error: true,
-        message: "Please provide all required salary details",
-      })
+    return res.status(400).send({
+      error: true,
+      message: "Please provide all required salary details",
+    })
   }
 
   dbConn.query(
@@ -279,12 +269,10 @@ app.post("/addSalary", function (req, res) {
     [userID, salary],
     function (error, results, fields) {
       if (error) {
-        return res
-          .status(500)
-          .send({
-            error: true,
-            message: "Error inserting salary into database",
-          })
+        return res.status(500).send({
+          error: true,
+          message: "Error inserting salary into database",
+        })
       }
       return res.send({
         error: false,
@@ -299,12 +287,10 @@ app.post("/addBonus", function (req, res) {
   const { userID, salaryID, bonus, bonusDate } = req.body
 
   if (!userID || !salaryID || !bonus || !bonusDate) {
-    return res
-      .status(400)
-      .send({
-        error: true,
-        message: "Please provide all required bonus details",
-      })
+    return res.status(400).send({
+      error: true,
+      message: "Please provide all required bonus details",
+    })
   }
 
   dbConn.query(
@@ -325,23 +311,213 @@ app.post("/addBonus", function (req, res) {
   )
 })
 app.post("/addEmployee", function (req, res) {
-      const { userID, emprank, reports_to, job_id, department, account_no } = req.body;
+  const { userID, emprank, reports_to, job_id, department, account_no } =
+    req.body
+
+  if (!userID || !emprank || !job_id || !department) {
+    return res.status(400).send({
+      error: true,
+      message: "Please provide all required employee details",
+    })
+  }
+
+  dbConn.query(
+    "INSERT INTO employee (userID, emprank, reports_to, job_id, department, account_no) VALUES (?, ?, ?, ?, ?, ?)",
+    [userID, emprank, reports_to, job_id, department, account_no],
+    function (error, results, fields) {
+      if (error) {
+        return res.status(500).send({
+          error: true,
+          message: "Error inserting employee into database",
+        })
+      }
+      return res.send({
+        error: false,
+        data: results,
+        message: "New employee record has been created successfully.",
+      })
+    }
+  )
+})
+
+app.post("/addTimeSheet", function (req, res) {
+  const { userID, checkinTime, checkoutTime, projectName, taskName, status } =
+    req.body
+
+  if (
+    !userID ||
+    !checkinTime ||
+    !checkoutTime ||
+    !projectName ||
+    !taskName ||
+    !status
+  ) {
+    return res.status(400).send({
+      error: true,
+      message: "Please provide all required timeSheet details",
+    })
+  }
+
+  dbConn.query(
+    "INSERT INTO timeSheet (userID, checkinTime, checkoutTime, projectName, taskName, status) VALUES (?, ?, ?, ?, ?, ?)",
+    [userID, checkinTime, checkoutTime, projectName, taskName, status],
+    function (error, results, fields) {
+      if (error) {
+        return res.status(500).send({
+          error: true,
+          message: "Error inserting timeSheet into database",
+        })
+      }
+      return res.send({
+        error: false,
+        data: results,
+        message: "New timeSheet record has been created successfully.",
+      })
+    }
+  )
+})
+
+app.post("/addEmployeeJobInfo", function (req, res) {
+  const {
+    userID,
+    jobName,
+    joiningDate,
+    cv,
+    passport,
+    healthCheck,
+    visa,
+    jobContract,
+    reportsTo,
+  } = req.body
+
+  if (
+    !userID ||
+    !jobName ||
+    !joiningDate ||
+    !cv ||
+    !passport ||
+    !healthCheck ||
+    !visa ||
+    !jobContract ||
+    !reportsTo
+  ) {
+    return res.status(400).send({
+      error: true,
+      message: "Please provide all required employeejobinfo details",
+    })
+  }
+
+  dbConn.query(
+    "INSERT INTO employeejobinfo (userID, jobName, joiningDate, cv, passport, healthCheck, visa, jobContract, reportsTo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    [
+      userID,
+      jobName,
+      joiningDate,
+      cv,
+      passport,
+      healthCheck,
+      visa,
+      jobContract,
+      reportsTo,
+    ],
+    function (error, results, fields) {
+      if (error) {
+        return res.status(500).send({
+          error: true,
+          message: "Error inserting employeejobinfo into database",
+        })
+      }
+      return res.send({
+        error: false,
+        data: results,
+        message: "New employeejobinfo record has been created successfully.",
+      })
+    }
+  )
+})
+app.post("/addEmployeeLeave", function (req, res) {
+  const { userID, date, status } = req.body
+
+  if (!userID || !date || !status) {
+    return res
+      .status(400)
+      .send({
+        error: true,
+        message: "Please provide all required empleave details",
+      })
+  }
+
+  dbConn.query(
+    "INSERT INTO empleave (userID, date, status) VALUES (?, ?, ?)",
+    [userID, date, status],
+    function (error, results, fields) {
+      if (error) {
+        return res
+          .status(500)
+          .send({
+            error: true,
+            message: "Error inserting empleave into database",
+          })
+      }
+      return res.send({
+        error: false,
+        data: results,
+        message: "New empleave record has been created successfully.",
+      })
+    }
+  )
+})
+
+app.post("/addAnonymousMessage", function (req, res) {
+  const { RecieveID, title, message, attachments } = req.body
+
+  if (!RecieveID || !title || !message) {
+    return res
+      .status(400)
+      .send({
+        error: true,
+        message: "Please provide all required anonymousMessages details",
+      })
+  }
+
+  dbConn.query(
+    "INSERT INTO anonymousMessages (RecieveID, title, message, attachments) VALUES (?, ?, ?, ?)",
+    [RecieveID, title, message, attachments],
+    function (error, results, fields) {
+      if (error) {
+        return res
+          .status(500)
+          .send({
+            error: true,
+            message: "Error inserting anonymousMessages into database",
+          })
+      }
+      return res.send({
+        error: false,
+        data: results,
+        message: "New anonymousMessages record has been created successfully.",
+      })
+    }
+  )
+})
+app.post("/addDirectMessage", function (req, res) {
+      const { senderID, RecieveID, title, message, attachments } = req.body;
     
-      if (!userID || !emprank || !job_id || !department) {
-        return res.status(400).send({ error: true, message: "Please provide all required employee details" });
+      if (!senderID || !RecieveID || !title || !message) {
+        return res.status(400).send({ error: true, message: "Please provide all required DirectMessages details" });
       }
     
       dbConn.query(
-        "INSERT INTO employee (userID, emprank, reports_to, job_id, department, account_no) VALUES (?, ?, ?, ?, ?, ?)",
-        [userID, emprank, reports_to, job_id, department, account_no],
+        "INSERT INTO DirectMessages (senderID, RecieveID, title, message, attachments) VALUES (?, ?, ?, ?, ?)",
+        [senderID, RecieveID, title, message, attachments],
         function (error, results, fields) {
           if (error) {
-            return res.status(500).send({ error: true, message: "Error inserting employee into database" });
+            return res.status(500).send({ error: true, message: "Error inserting DirectMessages into database" });
           }
           return res.send({
             error: false,
             data: results,
-            message: "New employee record has been created successfully.",
+            message: "New DirectMessages record has been created successfully.",
           });
         }
       );
